ec41aee0004308d1b62bc9bf0ec4db01cc0a5050
Nazila Valiyeva (Nazilə Vəliyeva) <Nazilaveliyeva94@gmail.com>
2025-06-17 13:34:51 +0400

    Add project files.

---
+++ c1/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace c1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMilliyyet = new System.Windows.Forms.TextBox();
+            this.txtTarix = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnElaveEt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(407, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 29);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Göstər";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(412, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 24);
+            this.comboBox1.TabIndex = 34;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 29);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "İstifadəçi milliyyəti";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(245, 29);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "İstifadəçi doğum tarixi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 29);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "İstifadəçi soyadı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 29);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "İstifadəçi adı";
+            // 
+            // txtMilliyyet
+            // 
+            this.txtMilliyyet.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtMilliyyet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMilliyyet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMilliyyet.Location = new System.Drawing.Point(17, 283);
+            this.txtMilliyyet.Multiline = true;
+            this.txtMilliyyet.Name = "txtMilliyyet";
+            this.txtMilliyyet.Size = new System.Drawing.Size(350, 32);
+            this.txtMilliyyet.TabIndex = 29;
+            // 
+            // txtTarix
+            // 
+            this.txtTarix.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtTarix.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTarix.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTarix.Location = new System.Drawing.Point(15, 211);
+            this.txtTarix.Multiline = true;
+            this.txtTarix.Name = "txtTarix";
+            this.txtTarix.Size = new System.Drawing.Size(350, 32);
+            this.txtTarix.TabIndex = 28;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtSoyad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoyad.Location = new System.Drawing.Point(15, 128);
+            this.txtSoyad.Multiline = true;
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(350, 32);
+            this.txtSoyad.TabIndex = 27;
+            // 
+            // txtAd
+            // 
+            this.txtAd.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtAd.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAd.Location = new System.Drawing.Point(15, 45);
+            this.txtAd.Multiline = true;
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(350, 32);
+            this.txtAd.TabIndex = 26;
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.BackColor = System.Drawing.Color.Red;
+            this.btnTemizle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTemizle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTemizle.Location = new System.Drawing.Point(201, 333);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(164, 35);
+            this.btnTemizle.TabIndex = 25;
+            this.btnTemizle.Text = "Təmizlə";
+            this.btnTemizle.UseVisualStyleBackColor = false;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnElaveEt
+            // 
+            this.btnElaveEt.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnElaveEt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnElaveEt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnElaveEt.Location = new System.Drawing.Point(17, 331);
+            this.btnElaveEt.Name = "btnElaveEt";
+            this.btnElaveEt.Size = new System.Drawing.Size(166, 37);
+            this.btnElaveEt.TabIndex = 24;
+            this.btnElaveEt.Text = "Əlavə et";
+            this.btnElaveEt.UseVisualStyleBackColor = false;
+            this.btnElaveEt.Click += new System.EventHandler(this.btnElaveEt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(594, 375);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMilliyyet);
+            this.Controls.Add(this.txtTarix);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnElaveEt);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "İstifadəçi paneli";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMilliyyet;
+        private System.Windows.Forms.TextBox txtTarix;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button btnElaveEt;
+    }
+}
+
---
+++ c1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnElaveEt_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                string secim = comboBox1.Text;
+                if (secim == "Ali Valiyev")
+                {
+                    txtAd.Text = "Ali";
+                    txtSoyad.Text = "Valiyev";
+                    txtTarix.Text = "11.04.2016";
+                    txtMilliyyet.Text = "Azerbaycanli";
+                }
+                else if(secim == "Elman Qasimov")
+                {
+                    txtAd.Text = "Elman";
+                    txtSoyad.Text = "Qasimov";
+                    txtTarix.Text = "15.04.2001";
+                    txtMilliyyet.Text = "Azerbaycanli";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Xeta bas verdi, xahiş edirik yenidən cəhd edin.", "Xəta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+         
+           
+
+            
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            txtAd.Clear();
+            txtSoyad.Clear();
+            txtTarix.Clear();
+            txtMilliyyet.Clear();   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Ali Valiyev");
+            comboBox1.Items.Add("Elman Qasimov");
+        }
+    }
+}
